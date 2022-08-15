812b969eaccb0a49655f11ff2b950f59403eee11
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-15 22:58:13 +0000

    feat(login): added test on the onchange event

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Login from './login/Login';
 
 test('username input should be rendered', () => {
@@ -13,8 +13,50 @@ test('password input should be rendered', () => {
   expect(passwordInputEl).toBeInTheDocument();
 });
 
-test('button input should be rendered', () => {
+test('button should be rendered', () => {
   render(<Login />);
   const buttonInputEl = screen.getByRole('button');
   expect(buttonInputEl).toBeInTheDocument();
+});
+
+test('username input should be empty', () => {
+  render(<Login />);
+  const userInputEl = screen.getByPlaceholderText(/username/i);
+  expect(userInputEl.value).toBe('');
+});
+
+test('password input should be empty', () => {
+  render(<Login />);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+  expect(passwordInputEl.value).toBe('');
+});
+
+test('button should be disabled', () => {
+  render(<Login />);
+  const buttonInputEl = screen.getByRole('button');
+  expect(buttonInputEl).toBeDisabled();
+});
+
+test('error message should not be disabled', () => {
+  render(<Login />);
+  const errorEl = screen.getByTestId('error');
+  expect(errorEl).not.toBeVisible();
+});
+
+test('username input should change', () => {
+  render(<Login />);
+  const userInputEl = screen.getByPlaceholderText(/username/i);
+  const testValue = 'test';
+  
+  fireEvent.change(userInputEl, { target: { value: testValue } });
+  expect(userInputEl.value).toBe(testValue);
+});
+
+test('password input should change', () => {
+  render(<Login />);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+  const testValue = 'test';
+  
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  expect(passwordInputEl.value).toBe(testValue);
 });
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+
 const Login = () => {
+  const [error, setError] = useState(false);
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
   return (
     <div className='container'>
       <form>
-        <input type='text' placeholder='username' />
-        <input type='password' placeholder='password' />
-        <button>Login</button>
+        <input
+          type='text'
+          placeholder='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button disabled='true'>Login</button>
+        <span
+          data-testid='error'
+          style={{ visibility: error ? 'visible' : 'hidden' }}
+        >
+          Something went wrong!
+        </span>
       </form>
     </div>
   );
