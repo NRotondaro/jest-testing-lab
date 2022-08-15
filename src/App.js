a4f1cf9c215c7224f17a3767eb534a87d74ea853
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-15 22:31:20 +0000

    changed the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://ultimateqa.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          data-testid='learn-link'
-        >
-          Learn Testing
-        </a>
-      </header>
+      <Login/>
     </div>
   );
 }
