2282d3ec5284b4ec744bbdbfe6e800314c0f7f9d
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-18 23:13:13 +0000

    feat(summary-form): added popover and tested

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Popover } from 'react-bootstrap';
+import { OverlayTrigger } from 'react-bootstrap';
 
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement='right' overlay={popover}>
+        <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
+
   return (
     <div>
       <Form>
