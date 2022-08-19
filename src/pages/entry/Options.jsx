ff593952c7527f32f450c7f8b3f6fa1866537e18
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-19 19:16:15 +0000

    feat(alert): test and code alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,21 +1,27 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Row } from 'react-bootstrap';
+import AlertBanner from '../common/AlertBanner';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 
 export default function Options({ optionTypes }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionTypes}`)
       .then((response) => setItems(response.data))
-      .catch((err) => {
-        // TODO: handle error response
+      .catch((error) => {
+        setError(true);
       });
   }, [optionTypes]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // TODO: replace 'null' with ToppingOption
   const ItemComponent = optionTypes === 'scoops' ? ScoopOption : ToppingOption;
