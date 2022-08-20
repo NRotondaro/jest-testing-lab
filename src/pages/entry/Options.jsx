b6d0851bc7be44bbbc7c6354bda274575df96f35
NRotondaro <niksonrotondaro1@gmail.com>
2022-08-20 23:27:31 +0000

    feat(options): using the created context on the options component and rendered app.js with the proper components

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,27 +4,31 @@ import { Row } from 'react-bootstrap';
 import AlertBanner from '../common/AlertBanner';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
+import { pricePerItem } from '../../constants';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
-export default function Options({ optionTypes }) {
+export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = useOrderDetails();
 
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
-      .get(`http://localhost:3030/${optionTypes}`)
+      .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
         setError(true);
       });
-  }, [optionTypes]);
+  }, [optionType]);
 
   if (error) {
     return <AlertBanner />;
   }
 
   // TODO: replace 'null' with ToppingOption
-  const ItemComponent = optionTypes === 'scoops' ? ScoopOption : ToppingOption;
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
+  const title = optionType[0].toUpperCase() + optionType.slice(1).toLowerCase();
 
   const optionItems = items.map((item) => {
     return (
@@ -32,13 +36,21 @@ export default function Options({ optionTypes }) {
         key={item.name}
         name={item.name}
         imagePath={item.imagePath}
+        updateItemCount={(itemName, newItemCount) =>
+          updateItemCount(itemName, newItemCount, optionType)
+        }
       />
     );
   });
 
   return (
-    <div>
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
       <Row>{optionItems}</Row>;
-    </div>
+    </>
   );
 }
